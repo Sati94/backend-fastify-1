2260179a49ac4f6151276fb1df5a35e2352422ce
Ati <nagyonkellvalamihez@gmail.com>
2024-11-14 10:54:04 +0100

    add a modfy function to the code. Create a PetToUpdate type, adding request HTTP for testing

--- src/controller/pet-routes.ts
+++ src/controller/pet-routes.ts
@@ -1,6 +1,8 @@
 import { petSchema } from '../entity/schema/pet-schema';
 import { PetToCreate } from '../entity/pet.type';
+import { PetDataUpdate } from '../entity/pet.type';
 import { FastifyInstance } from 'fastify';
+import { petToUpdateSchema } from '../entity/schema/pet-update';
 
 
 export async function petRoutes(app: FastifyInstance) {
@@ -18,4 +20,20 @@ export async function petRoutes(app: FastifyInstance) {
         reply.status(201);
         return createdPet;
     });
+
+    app.patch<{ Params: { id: number }; Body: PetDataUpdate }>('/api/pets/:id', {
+        schema: petToUpdateSchema
+    }, async (request, reply) => {
+        const petId = request.params.id;
+        const petData = request.body;
+
+        const updatedPet = await app.petService.modifyPet(petId, petData);
+
+        if (!updatedPet) {
+            reply.status(404).send({ message: 'Pet not found or no data to update' });
+            return;
+        }
+        reply.status(200).send(updatedPet);
+
+    })
 }
